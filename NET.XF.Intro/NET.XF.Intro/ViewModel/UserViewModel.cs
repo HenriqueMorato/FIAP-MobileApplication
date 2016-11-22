55c3cc90c30ed4db8de1297f594f73b54015541a
MOB_UWP <MOB_UWP@DESKTOP-E2MRBIP>
2016-11-21 21:31:24 -0800

    Model, ViewModel

---
+++ NET.XF.Intro/NET.XF.Intro/ViewModel/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using NET.XF.Intro.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.XF.Intro.ViewModel
+{
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<User> Users;
+
+        public UserViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
