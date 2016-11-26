803d6454126181f4b7fc89859d2fa0e78618b9c4
MOB_UWP <MOB_UWP@DESKTOP-E2MRBIP>
2016-11-26 04:58:44 -0800

    Novo projeto inicial - Aula sábado

---
+++ MOB.XF.MVVMBasic/MOB.XF.MVVMBasic/View/AlunoViewPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MOB.XF.MVVMBasic.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MOB.XF.MVVMBasic.View
+{
+    public partial class AlunoViewPage : ContentPage
+    {
+        AlunoViewModel vmAluno;
+        public AlunoViewPage()
+        {
+            var aluno = AlunoViewModel.GetAluno();
+            vmAluno = new AlunoViewModel(aluno);
+            BindingContext = vmAluno;
+            InitializeComponent();
+        }
+    }
+}
